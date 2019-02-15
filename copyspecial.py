70a89a2ffb098cb63d190421a3133c3a3e8a5a4c
Doug Enas <douglassenas@gmail.com>
2019-02-15 10:58:33 -0500

    special paths function works

--- copyspecial.py
+++ copyspecial.py
@@ -9,36 +9,53 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
+
+
+
+__author__ = "dougenas"
+
+
 import re
 import os
 import shutil
 import subprocess
 import argparse
-
-# This is to help coaches and graders identify student assignments
-__author__ = "???"
+import sys
 
 
-# +++your code here+++
-# Write functions and modify main() to call them
+__author__ = "dougenas"
+
+
+def get_special_paths(dir):
+    special_paths = []
+    files = os.listdir(dir)
+    for file in files:
+        if re.search(r'__\w+__', file):
+            special_paths.append(file)
+    return special_paths
+
+
+def copy_to(paths, dir):
+    print('copy_to', paths, dir)
+
+
+def zip_to(paths, zippath):
+    print('zip_to', paths, zippath)
+
 
 def main():
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO need an argument to pick up 'from_dir'
+    parser.add_argument('fromdir', help='src dir to look for local files')
     args = parser.parse_args()
 
-    # TODO you must write your own code to get the cmdline args.
-    # Read the docs and examples for the argparse module about how to do this.
+    print(args)
 
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation.  If something is wrong (or missing) with any
-    # required args, the general rule is to print a usage message and exit(1).
-
-    # +++your code here+++
-    # Call your functions
+    if not args.todir and not args.tozip:
+        for file in get_special_paths(args.fromdir):
+            print(os.path.abspath(file))
 
 
 if __name__ == "__main__":
